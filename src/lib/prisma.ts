227ce1ffee30397b1abfb1fa1659c6a0c24fb481
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2023-11-22 16:49:00 +0700

    ✨feat: Initial Schema

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export const User = prisma.User;
+export const Session = prisma.session;
+export const User = prisma.user;
+export const Project = prisma.project;
+export const SDG = prisma.sDG;
