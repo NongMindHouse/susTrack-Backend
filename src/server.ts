333de4bdabb0ffcbc44cb895214bbe530925e01a
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2023-11-26 11:32:55 +0700

    add cors

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ const app: Express = express();
 const port = process.env.PORT || 8080;
 
 app.use(helmet());
+app.use(cors())
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
