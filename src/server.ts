92ddb0a1223354e40c769aae2a1fdc3d7b300bd1
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2023-11-26 10:39:34 +0700

    remove cors

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const app: Express = express();
 const port = process.env.PORT || 8080;
 
 app.use(helmet());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser())
